8626d3d0a87e73d7862daa474c222ad9ec1dea77
Gino Burdea <burdea.gino@gmail.com>
2024-05-03 12:36:21 +0300

    fix: add better validation for names and usernames

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -2,8 +2,26 @@ import Joi from 'joi'
 
 export const authValidators = {
     register: Joi.object({
-        name: Joi.string().min(4).max(64).required(),
-        username: Joi.string().min(6).max(64).required(),
+        name: Joi.string()
+            .min(4)
+            .max(64)
+            .trim()
+            .pattern(/^[a-z ']+$/i)
+            .required()
+            .messages({
+                'string.pattern.base':
+                    '"name" can only contain letters, spaces and apostrophes',
+            }),
+        username: Joi.string()
+            .min(6)
+            .max(64)
+            .trim()
+            .pattern(/^[a-z0-9_\.]+$/)
+            .required()
+            .messages({
+                'string.pattern.base':
+                    '"username" can only contain lowercase letters, numbers, underscores (_), and dots (.)',
+            }),
         password: Joi.string().min(8).max(64).required(),
     }),
     login: Joi.object({
