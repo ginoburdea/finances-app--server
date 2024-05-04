565bf998451f0bb82080544cb995ab1f218bc716
Gino Burdea <burdea.gino@gmail.com>
2024-05-05 00:33:35 +0300

    fix: make sure the entry sums accept floats not ints

--- src/validators/entries.js
+++ src/validators/entries.js
@@ -5,7 +5,7 @@ export const entriesValidators = {
     addEntry: Joi.object({
         date: Joi.date().iso().required(),
         categoryId: Joi.string().pattern(mongoIdPattern, 'id').required(),
-        sum: Joi.number().min(0.01).precision(2).required(),
+        sum: Joi.number().min(0.01).multiple(0.01).required(),
     }),
     getEntries: Joi.object({
         date: Joi.date().iso().required(),
